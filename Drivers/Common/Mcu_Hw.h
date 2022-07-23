fa93e8dceb74a4a860fc819b0c497f5dbe43a634
abdurrahman26 <abdurrahmanma01@gmail.com>
2022-07-23 17:31:14 +0200

    modified GPIODATA Adderss to match with keil

--- Drivers/Common/Mcu_Hw.h
+++ Drivers/Common/Mcu_Hw.h
@@ -24,8 +24,7 @@ typedef struct
     uint32 VECACT   :8;
     uint32          :3;
     uint32 RETBASE  :1;
-    uint32 VECPEND  :3;
-    uint32 VECPEND  :4;
+    uint32 VECPEND  :7;
     uint32          :2;
     uint32 ISRPEND  :1;
     uint32 ISRPRE   :1;
@@ -128,6 +127,7 @@ typedef union
  *  GLOBAL CONSTANT MACROS
  *********************************************************************************************************************/
 #define CORTEXM4_PERI_BASE_ADDRESS             0xE000E000
+#define SYSTEM_CONTROL_BASE_ADDRESS            0x400FE000
 #define APINT                                  (*((volatile uint32*)(CORTEXM4_PERI_BASE_ADDRESS+0xD0C)))
 #define INTCTRL                                (*((volatile INTCTRL_Tag*)(CORTEXM4_PERI_BASE_ADDRESS+0xD04)))
 #define SYSPRI1                                (*((volatile SYSPRI1_U*)(CORTEXM4_PERI_BASE_ADDRESS+0xD18)))
@@ -142,11 +142,88 @@ typedef union
 
 
 /***********GPIO Registers***********/
-#define GPIODATAx_OFFSET                       0X000
+#define RCGCGPIO                            (*((volatile uint32*)(SYSTEM_CONTROL_BASE_ADDRESS+0x608)))
+
+#define GPIODATAx_OFFSET                       0X3FC
 #define GET_GPIODATAx_ADDRESS(Base)            (*((volatile uint32*)(GPIODATAx_OFFSET+Base)))
 
-#define GPIODIRx_OFFSET                       0X400
-#define GET_GPIODIRx_ADDRESS(Base)            (*((volatile uint32*)(GPIODIRx_OFFSET+Base)))
+#define GPIODIRx_OFFSET                        0X400
+#define GET_GPIODIRx_ADDRESS(Base)             (*((volatile uint32*)(GPIODIRx_OFFSET+Base)))
+
+//0x4C4F434B 
+#define GPIOLOCKx_OFFSET                       0x520
+#define GPIOLOCKx(Base)             (*((volatile uint32*)(GPIOLOCKx_OFFSET+Base)))
+
+
+#define GPIOCRx_OFFSET                         0x524
+#define GPIOCRx(Base)             (*((volatile uint32*)(GPIOCRx_OFFSET+Base)))
+
+#define GPIOAFSELx_OFFSET                      0x420
+#define GPIOAFSELx(Base)             (*((volatile uint32*)(GPIOAFSELx_OFFSET+Base)))
+
+
+#define GPIOPCTLx_OFFSET                       0x52C
+#define GPIOPCTLx(Base)             (*((volatile uint32*)(GPIOPCTLx_OFFSET+Base)))
+
+
+#define GPIODR2Rx_OFFSET                       0x500
+#define GPIODR2Rx(Base)             (*((volatile uint32*)(GPIODR2Rx_OFFSET+Base)))
+
+#define GPIODR4Rx_OFFSET                       0x504
+#define GPIODR4Rx(Base)             (*((volatile uint32*)(GPIODR4Rx_OFFSET+Base)))
+
+#define GPIODR8Rx_OFFSET                       0x508
+#define GPIODR8Rx(Base)             (*((volatile uint32*)(GPIODR8Rx_OFFSET+Base)))
+
+#define GPIOODRx_OFFSET                       0x50C
+#define GPIOODRx(Base)             (*((volatile uint32*)(GPIOODRx_OFFSET+Base)))
+
+#define GPIOODRx_OFFSET                       0x50C
+#define GPIOODRx(Base)             (*((volatile uint32*)(GPIOODRx_OFFSET+Base)))
+
+#define GPIOPURx_OFFSET                       0x510
+#define GPIOPURx(Base)             (*((volatile uint32*)(GPIOPURx_OFFSET+Base)))
+
+#define GPIOPDRx_OFFSET                       0x514
+#define GPIOPDRx(Base)             (*((volatile uint32*)(GPIOPDRx_OFFSET+Base)))
+
+#define GPIODENx_OFFSET                       0x51C
+#define GPIODENx(Base)             (*((volatile uint32*)(GPIODENx_OFFSET+Base)))
+
+
+
+
+/*******************************************************
+ *********************GPT Registers********************
+ *******************************************************/
+
+#define GPTMCFG_OFFSET                       0x000
+#define GPTMCFG(Base)             (*((volatile uint32*)(GPTMCFG_OFFSET+Base)))
+
+#define GPTMTAMR_OFFSET                       0x004
+#define GPTMTAMR(Base)             (*((volatile uint32*)(GPTMTAMR_OFFSET+Base)))
+
+#define GPTMCTL_OFFSET                       0x00C
+#define GPTMCTL(Base)             (*((volatile uint32*)(GPTMCTL_OFFSET+Base)))
+
+#define GPTMIMR_OFFSET                       0x018
+#define GPTMIMR(Base)             (*((volatile uint32*)(GPTMIMR_OFFSET+Base)))
+
+#define GPTMRIS_OFFSET                       0x01C
+#define GPTMRIS(Base)             (*((volatile uint32*)(GPTMRIS_OFFSET+Base)))
+
+#define GPTMMIS_OFFSET                       0x020
+#define GPTMMIS(Base)             (*((volatile uint32*)(GPTMMIS_OFFSET+Base)))
+
+#define GPTMTAV_OFFSET                       0x050
+#define GPTMTAV(Base)             (*((volatile uint32*)(GPTMTAV_OFFSET+Base)))
+
+#define GPTMTAILR_OFFSET                     0x028
+#define GPTMTAILR(Base)             (*((volatile uint32*)(GPTMTAILR_OFFSET+Base)))
+
+
+
+
 
 
 /**********************************************************************************************************************
